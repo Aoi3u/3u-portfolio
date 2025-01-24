f3838202ff11ebad3f3089d74370a013074caccb
Aoi3u <miu.blue3@gmail.com>
2025-01-24 15:54:42 +0900

    アイコンの実装・文の変更

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.documentElement.classList.toggle(
+  "dark",
+  localStorage.currentTheme == "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme:dark)").matches)
+);
+
+localStorage.currentTheme = "light";
+localStorage.currentTheme = "dark";
+localStorage.removeItem("theme");
